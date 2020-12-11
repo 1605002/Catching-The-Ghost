c711c1a924d1c69e0b317580be386a240ddeb999
cgspyn868 <1605002@ugrad.cse.buet.ac.bd>
2020-12-12 02:13:00 +0600

    Submittable offline

--- js/scripts.js
+++ js/scripts.js
@@ -59,11 +59,13 @@ for(let farp of sensorFarps) {
 }
 
 let n, m, g;
+let near, far;
 let belief;
 let ghostsx, ghostsy;
 let dirx = [-1, 1, 0, 0, -1, 1, 1, -1, 0];
 let diry = [0, 0, -1, 1, -1, -1, 1, 1, 0];
-let movep, transp;
+let movep, transp, sensorp;
+let sensorState;
 
 let innerVal = id => document.getElementById(id).value;
 let innerValp = id => innerVal(id)/100;
@@ -78,6 +80,8 @@ let initialize = () => {
     n = document.getElementById("rows").value;
     m = document.getElementById("cols").value;
     g = document.getElementById("ghosts").value;
+
+    near = document.getElementById("near").value, far = document.getElementById("far").value;
 
     let bcont = document.getElementById("board-container");
     bcont.innerHTML = "";
@@ -156,7 +160,78 @@ let initialize = () => {
         }
     }
 
-    console.log(transp[0][0][0][1]);
+    sensorp = [[], [], []];
+    let fw = ["close", "medium", "far"], sw = ["red", "orange", "green"];
+
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) sensorp[i][j] = document.getElementById(`${fw[i]}-${sw[j]}-p`).value/100;
+    }
+
+    let getTmpBoard = () => {
+        let tmpBoard = [];
+        for(let i = 0; i < n; i++) {
+            tmpBoard.push([]);
+            for(let j = 0; j < m; j++) tmpBoard[i].push(0);
+        }
+
+        return tmpBoard;
+    }
+
+    let normalize = () => {
+        let sump = 0;
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < m; j++) sump += belief[i][j];
+        }
+
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < m; j++) belief[i][j] /= sump;
+        }
+    };
+
+    let setTextTable = () => {
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < m; j++)
+                document.getElementById(`cell-${i}-${j}`).textContent = (belief[i][j]*100).toFixed(2);
+        }
+    }
+
+    let whichd = d => {
+        console.assert(d >= 0);
+        
+        if(d <= near) return 0;
+        if(d <= far) return 1;
+        return 2;
+    };
+
+    let getDist = (i1, j1, i2, j2) => Math.abs(i1-i2)+Math.abs(j1-j2); 
+
+    let sense = () => {
+        sensorState = [];
+        for(let i = 0; i < n; i++) sensorState.push([]);
+
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < m; j++) {
+                let d = n+m;
+
+                for(let k = 0; k < g; k++) {
+                    let x = ghostsx[k], y = ghostsy[k];
+                    if(getDist(i, j, x, y) < d) d = getDist(i, j, x, y);
+                }
+
+                let cat = whichd(d);
+
+                let r = Math.random();
+
+                if(r < sensorp[cat][0]) sensorState[i][j] = 0;
+                else if(r < sensorp[cat][0]+sensorp[cat][1]) sensorState[i][j] = 1;
+                else sensorState[i][j] = 2;
+            }
+        }
+
+        console.log(sensorState);
+    };
+
+    sense();
 
     let timeButton = document.createElement("button");
     timeButton.id = "time-advance";
@@ -165,6 +240,12 @@ let initialize = () => {
     bcont.appendChild(timeButton);
 
     timeButton.addEventListener("click", () => {
+        if(g == 0) return;
+        cotg.textContent = "";
+
+        onCatch = false;
+        catchButton.style.backgroundColor = "initial";
+
         for(let k = 0; k < g; k++) {
             let i1 = ghostsx[k], j1 = ghostsy[k];
             let r = Math.random(), ald = 0;
@@ -181,14 +262,13 @@ let initialize = () => {
             }
         }
 
-        //console.log(ghostsx);
-        //console.log(ghostsy);
+        sense();
 
-        let tmpBoard = [];
         for(let i = 0; i < n; i++) {
-            tmpBoard.push([]);
-            for(let j = 0; j < m; j++) tmpBoard[i].push(0);
+            for(let j = 0; j < m; j++) document.getElementById(`cell-${i}-${j}`).className = ""; // May be will change
         }
+
+        let tmpBoard = getTmpBoard();
 
         for(let i2 = 0; i2 < n; i2++) {
             for(let j2 = 0; j2 < m; j2++) {
@@ -218,23 +298,118 @@ let initialize = () => {
     bcont.appendChild(catchButton);
 
     catchButton.addEventListener("click", () => {
+        if(g == 0) return;
+        cotg.textContent = "";
+
         onCatch = !onCatch;
 
         if(onCatch) catchButton.style.backgroundColor = "#eb867f";
         else catchButton.style.backgroundColor = "initial";
     });
 
-    let handleCatch = (i, j) => {
-        console.log(`Tried to catch in (${i}, ${j})`);
+    let remg = document.createElement("div");
+    remg.className = "game-text";
+    remg.textContent = `Remaining ghosts: ${g}`;
+    bcont.appendChild(remg);
+
+    let cotg = document.createElement("div");
+    cotg.className = "game-text";
+    bcont.appendChild(cotg);
+
+    let endg = document.createElement("div");
+    endg.className = "game-text";
+    bcont.appendChild(endg);
+
+    let handleCatch = (x, y) => {
+        console.log(`Tried to catch in (${x}, ${y})`);
+
+        let tmpgx = [], tmpgy = [];
+        for(let k = 0; k < g; k++) {
+            let gx = ghostsx[k], gy = ghostsy[k];
+            if(gx != x || gy != y) {
+                tmpgx.push(gx);
+                tmpgy.push(gy);
+            }
+        }
+
+        cg = ghostsx.length-tmpgx.length;
+        ghostsx = [...tmpgx], ghostsy = [...tmpgy];
+        g -= cg;
+
+        remg.textContent = `Remaining ghosts: ${g}`;
+
+        let txt = "No ghost was in that cell";
+        if(cg === 1) txt = "1 ghost caught!"
+        else txt = `${cg} ghosts caught!`;
+
+        cotg.textContent = txt;
+
+        if(g == 0) {
+            endg.textContent = "You have won but at what cost";
+            return;
+        }
+
+        belief[x][y] = 0;
+
+        normalize();
+        setTextTable();
     }
 
-    let handleSensor = (i, j) => {
-        console.log(`Tried to sense in (${i}, ${j})`);
+    let handleSensor = (x, y) => {
+        console.log(`Tried to sense in (${x}, ${y})`);
+
+        let curClass = `state-${sensorState[x][y]}`;
+
+        let cell = document.getElementById(`cell-${x}-${y}`);
+        if(cell.className == curClass) return;
+
+        cell.className = curClass;
+        let tmpBoard = getTmpBoard();
+
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < m; j++) {
+                let nearp = 0, nearFarp = 0, selfCat = whichd(getDist(i, j, x, y));;
+
+                for(let ni = 0; ni < n; ni++) {
+                    for(let nj = 0; nj < m; nj++) {
+                        let cat = whichd(getDist(ni, nj, x, y));
+
+                        if(cat < 2) nearFarp += belief[ni][nj];
+                        if(cat < 1) nearp += belief[ni][nj];
+                    }
+                }
+
+                nearp = 1-nearp, nearFarp = 1-nearFarp;
+                nearp **= g-1, nearFarp **= g-1;
+                nearp = 1-nearp, nearFarp = 1-nearFarp;
+
+                let evidencep = 0, col = sensorState[x][y];
+
+                if(selfCat == 0) evidencep += sensorp[0][col];
+                else evidencep += nearp*sensorp[0][col];
+
+                if(selfCat == 1) evidencep += (1-nearp)*sensorp[1][col];
+                else if(selfCat == 2) evidencep += (nearFarp-nearp)*sensorp[1][col];
+
+                if(selfCat == 2) evidencep += (1-nearFarp)*sensorp[2][col];
+
+                tmpBoard[i][j] = belief[i][j]*evidencep;
+            }
+        }
+
+        for(let i = 0; i < n; i++) {
+            for(let j = 0; j < m; j++) belief[i][j] = tmpBoard[i][j];
+        }
+
+        normalize();
+        setTextTable();
     }
 
     for(let i = 0; i < n; i++) {
         for(let j = 0; j < m; j++) {
             document.getElementById(`cell-${i}-${j}`).addEventListener("click", e => {
+                if(g == 0) return;
+
                 let s = e.target.id.split("-");
                 let i = Number(s[1]), j = Number(s[2]);
 
